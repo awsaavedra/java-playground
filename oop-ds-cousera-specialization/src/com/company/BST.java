babc4e7ba16e013808c9cdaafdecfc93d8d24a07
Alexander Saavedra <awsaavedra@gmail.com>
2018-12-06 08:02:32 -0800

    BST basics and notes

--- oop-ds-cousera-specialization/src/com/company/BST.java
+++ oop-ds-cousera-specialization/src/com/company/BST.java
@@ -1,6 +1,27 @@
 package com.company;
 
-public class BST {
-    public TreeNode root;
 
+//Pre-order traversal (depth first traversal)
+//PostOrder traversal
+//InOrder traversal
+//Level order traversal (breadth first traversal)
+
+import java.util.Queue;
+
+public class BST {
+//    public TreeNode<E> root;
+//
+//    public void levelOrder(){
+//        Queue<TreeNode<E>> q = new LinkedList< TreeNode<E> >();
+//        q.add(root);
+//
+//        while(!q.isEmpty()) {
+//            TreeNode<E> curr = q.remove();
+//            if(curr != null){
+//                curr.visit();
+//                q.add(curr.getLeftChild());
+//                q.add(curr.getRightChild());
+//            }
+//        }
+//    }
 }
