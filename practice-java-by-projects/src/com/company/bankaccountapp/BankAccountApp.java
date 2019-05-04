72d88bef5edb42d16a20ddba67e7f3bb6b3233b3
Alexander Saavedra <awsaavedra@gmail.com>
2019-05-03 17:50:21 -0700

    finished, read in CSV, able to print out the info in them

--- practice-java-by-projects/src/com/company/bankaccountapp/BankAccountApp.java
+++ practice-java-by-projects/src/com/company/bankaccountapp/BankAccountApp.java
@@ -1,5 +1,6 @@
 package com.company.bankaccountapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
@@ -11,6 +12,8 @@ public class BankAccountApp {
     String accountNumber;
 
     public static void main(String[] args) {
+
+        List<Account> accounts = new LinkedList<Account>();
 
 //
 //        Checking checkingAccount1 = new Checking("Tom Wilson", "12345676", 1500);
@@ -28,13 +31,24 @@ public class BankAccountApp {
         List<String[]> newAccountHolders = utilities.CSV.read(file);
         for(String[] accountHolder: newAccountHolders) {
             System.out.println("NEW ACCOUNT");
-            System.out.println(accountHolder[0]);
-            System.out.println(accountHolder[1]);
-            System.out.println(accountHolder[2]);
-            System.out.println(accountHolder[3]);
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
 
+
+            if( accountType.equals("Savings")){
+                accounts.add(new Savings(name, sSN, initDeposit));
+            }else if( accountType.equals("Checking")){
+                accounts.add(new Checking(name, sSN, initDeposit));
+            }else{
+                System.out.println("ERROR READING ACCOUNT TYPE");
+            }
         }
 
+        for( Account account: accounts){
+            System.out.println("\n*********");
+            account.showInfo();
+        }
     }
-
 }
