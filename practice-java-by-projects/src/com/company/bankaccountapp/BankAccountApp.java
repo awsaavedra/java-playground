709d361662493466daafe5fd5b31b7fb98fb815f
Alexander Saavedra <awsaavedra@gmail.com>
2019-05-03 17:38:48 -0700

    added CSV reader, tested and got it working

--- practice-java-by-projects/src/com/company/bankaccountapp/BankAccountApp.java
+++ practice-java-by-projects/src/com/company/bankaccountapp/BankAccountApp.java
@@ -1,5 +1,7 @@
 package com.company.bankaccountapp;
 
+import java.util.List;
+
 public class BankAccountApp {
 
     String name;
@@ -10,16 +12,28 @@ public class BankAccountApp {
 
     public static void main(String[] args) {
 
-        Checking checkingAccount1 = new Checking("Tom Wilson", "12345676", 1500);
-        Savings savingsAccount1 = new Savings("Rich Lowe" , "12345676", 2500);
-
-        savingsAccount1.compound();
-
-        checkingAccount1.showInfo();
-        System.out.println("***********");
-        savingsAccount1.showInfo();
+//
+//        Checking checkingAccount1 = new Checking("Tom Wilson", "12345676", 1500);
+//        Savings savingsAccount1 = new Savings("Rich Lowe" , "12345676", 2500);
+//
+//        savingsAccount1.compound();
+//
+//        checkingAccount1.showInfo();
+//        System.out.println("***********");
+//        savingsAccount1.showInfo();
 
         // Read a CSV File then create new accounts based on that data
+
+        String file = "/Users/Alexander/workspace/java-playground/practice-java-by-projects/src/com/company/bankaccountapp/NewBankAccounts.csv";
+        List<String[]> newAccountHolders = utilities.CSV.read(file);
+        for(String[] accountHolder: newAccountHolders) {
+            System.out.println("NEW ACCOUNT");
+            System.out.println(accountHolder[0]);
+            System.out.println(accountHolder[1]);
+            System.out.println(accountHolder[2]);
+            System.out.println(accountHolder[3]);
+
+        }
 
     }
 
