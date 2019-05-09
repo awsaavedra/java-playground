5c4faaeecbad2a10a057a57594299121738ce505
Alexander Saavedra <awsaavedra@gmail.com>
2019-05-09 16:51:56 -0700

    expressions and statements

--- java-11-learning/src/com/company/Methods.java
+++ java-11-learning/src/com/company/Methods.java
@@ -6,18 +6,29 @@ public class Methods {
 
         boolean gameOver = true;
         int score = 800;
-        int levelCompleted = 5;
+        int lvlCompleted = 5;
         int bonus = 100;
 
-        calculateScore(true, 800, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver,score, lvlCompleted, bonus);
+
+        score = 10000;
+        lvlCompleted = 8;
+        bonus = 200;
+
+        highScore = calculateScore(gameOver, score, lvlCompleted, bonus); // adding these: gameOver, score, etc. are called arguments here
+        System.out.println("Your final score was " + highScore);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){ // gameOver, score, etc are parameters
 
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
+
+        return -1; // conventionally used to return an error.
+
     }
 }
