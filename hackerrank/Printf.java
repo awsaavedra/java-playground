77e754e5fb5ced871f09f31dec4ed019dc83a4a8
Alexander Saavedra <awsaavedra@gmail.com>
2019-08-21 16:32:28 -0700

    more basic hackerrank problems for warm-up

---
+++ hackerrank/Printf.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Printf{
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                System.out.printf("%-14s %03d", s1, x);
+                System.out.println();
+            }
+
+            System.out.println("================================");
+
+    }
+}
+
+
+
+
